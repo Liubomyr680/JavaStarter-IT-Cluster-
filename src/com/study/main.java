731e284862ee672fc926203c7183c386c1adf145
Liubomyr680 <lshynkaruk8@gmail.com>
2020-04-10 11:38:10 +0300

    Homework finished

---
+++ src/com/study/main.java
@@ -0,0 +1,15 @@
+package com.study;
+
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter text: ");
+        String str = scanner.nextLine();
+
+        JavaBuilder.javaSearch(str);
+    }
+}

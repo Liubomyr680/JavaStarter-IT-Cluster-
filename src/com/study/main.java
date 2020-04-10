268f373af185214d5ca7f675a4aa04bac3e900ea
Liubomyr680 <lshynkaruk8@gmail.com>
2020-04-10 11:54:47 +0300

    Homework finished

--- src/com/study/main.java
+++ src/com/study/main.java
@@ -6,11 +6,13 @@ public class main {
 
     public static void main(String[] args) {
 
+        //Програма у введеному рядку шукає тільки слово java,
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter text: ");
         String str = scanner.nextLine();
 
         JavaBuilder.javaSearch(str);
-        
+
     }
 }
